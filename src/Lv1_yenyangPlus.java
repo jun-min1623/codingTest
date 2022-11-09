9c6107c41d661dc5fcc989dd98f8427173659e59
Junmin <junmin1623@naver.com>
2022-11-09 14:49:04 +0900

    음양더하기

--- src/Lv1_yenyangPlus.java
+++ src/Lv1_yenyangPlus.java
@@ -1,2 +1,20 @@
 public class Lv1_yenyangPlus {
+    static class Solution {
+        public float ret_num(int num, boolean bool){
+            if(bool){
+                return num;
+            }else{
+                return num*(-1);
+            }
+        }
+        public int solution(int[] absolutes, boolean[] signs) {
+
+            int answer = 0;
+            for(int i=0;i<absolutes.length;i++){
+                answer += ret_num(absolutes[i],signs[i]);
+            }
+            return answer;
+        }
+    }
+
 }
