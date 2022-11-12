1af2135c30b3852051595735b39d5be35e9ae6a2
Junmin <junmin1623@naver.com>
2022-11-12 16:27:48 +0900

    feat(problem1_1): sol, test 추가

--- src/inflearn_java/problem1_1.java
+++ src/inflearn_java/problem1_1.java
@@ -3,11 +3,31 @@ package inflearn_java;
 import java.util.Scanner;
 
 public class problem1_1 {
-    public static void main(String[] args){
-        Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
-        int input2 = in.nextInt();
-        System.out.println(input1 + input2);
-        return ;
+    public static int solution(String str,char t){
+        int answer=0;
+        str = str.toUpperCase();
+        t=Character.toUpperCase(t);
+        for(int i=0; i<str.length();i++){
+            if(str.charAt(i) == t){
+                answer++;
+            }
+        }
+        return answer;
+    }
+    public long main(String args, Character a){
+        String str;
+        char c;
+        if(args.length()>0){
+            str = args;
+            c = a;
+        }else{
+
+            Scanner in=new Scanner(System.in);
+            str = in.next();
+            c = in.next().charAt(0);
+        }
+
+        System.out.print(solution(str,c));
+        return solution(str,c);
     }
 }
--- src/inflearn_java_test/inflearn_java/problem1_1Test.java
+++ src/inflearn_java_test/inflearn_java/problem1_1Test.java
@@ -2,11 +2,27 @@ package inflearn_java;
 
 import static org.junit.Assert.*;
 
+import inflearn_java.problem1_1.*;
+import main.Main;
 import org.junit.Test;
 
 public class problem1_1Test {
 
     @Test
     public void main() {
+        problem1_1 problem1_1 = new problem1_1();
+
+        String com = "Computercooler";
+        char c = 'c';
+        assertEquals(problem1_1.main(com,c),2);
+
+
     }
+
+    @Test
+    public void solution() {
+
+    }
+
+
 }
